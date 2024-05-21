11f3f8cbd35e64e5d0291ae70c09deebec5a1274
rrahn <rene.rahn@fu-berlin.de>
2024-05-21 20:37:37 +0200

    Add initial client-side routing with react to the App file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { Link, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import LockerOrderForm from './components/LockerOrderForm';
+import ErrorPage from './components/ErrorPage';
+import { basename } from 'path';
 
-const App: React.FC = () => {
-
+function Main() {
   return (
     <div>
-      <LockerOrderForm handleSubmit={() => console.log("Got some user data!")} />
+      <h1>Willkommen zum Schließfachsystem</h1>
+      <Link to="buchen">Schließfach buchen</Link>
+      <Outlet />
     </div>
   );
-};
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "buchen",
+        element: <LockerOrderForm handleSubmit={() => console.log("Got some user data!")} />,
+      },
+      ],
+  },
+], {
+  basename: "/Schliessfaecher", // This is the base URL of the app.
+});
+
+// function App() {
+//   return (
+//     <div>
+//       <LockerOrderForm handleSubmit={() => console.log("Got some user data!")} />
+//     </div>
+//   );
+// };
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
