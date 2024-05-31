f6f4f1519ed92feb7a2d5a94dae9001fb6e8110e
rrahn <rene.rahn@fu-berlin.de>
2024-05-31 09:54:27 +0200

    Adds API mocks for Locker story.

--- app/client/components/Locker.stories.tsx
+++ app/client/components/Locker.stories.tsx
@@ -1,4 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { http, HttpResponse, delay } from 'msw';
+
+
 import Locker from "./Locker";
 import { LockerSelectionProvider } from '../contexts/LockerSelectionContext';
 
@@ -18,14 +21,58 @@ const meta: Meta<typeof Locker> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+// 👇 The mocked data that will be used in the story
+const TestDataKeyLocker = [
+    { id: 1, kind: 'key', state: 'Free' },
+    { id: 2, kind: 'key', state: 'Free' },
+    { id: 3, kind: 'key', state: 'Free' },
+    { id: 4, kind: 'key', state: 'Free' },
+    { id: 5, kind: 'key', state: 'Free' },
+    { id: 6, kind: 'key', state: 'Free' },
+    { id: 7, kind: 'key', state: 'Free' },
+    { id: 8, kind: 'key', state: 'Free' },
+    { id: 9, kind: 'key', state: 'Free' },
+];
+
 export const KeyLockerTest : Story = {
+    parameters: {
+        msw: {
+            handlers: [
+                http.get('/api/getLockers/unit/1', () => {
+                    return HttpResponse.json(TestDataKeyLocker);
+                }),
+            ],
+        },
+    },
     args: {
-        id: 10,
+        id: 1,
     },
 };
 
+const TestDataCombinationLocker = [
+    { id: 1, kind: 'digit', state: 'Free' },
+    { id: 2, kind: 'digit', state: 'Free' },
+    { id: 3, kind: 'digit', state: 'Free' },
+    { id: 4, kind: 'digit', state: 'Free' },
+    { id: 5, kind: 'digit', state: 'Free' },
+    { id: 6, kind: 'digit', state: 'Free' },
+    { id: 7, kind: 'digit', state: 'Free' },
+    { id: 8, kind: 'digit', state: 'Free' },
+    { id: 9, kind: 'digit', state: 'Free' },
+    { id: 10, kind: 'digit', state: 'Free' },
+];
+
 export const DigitLockerTest : Story = {
+    parameters: {
+        msw: {
+            handlers: [
+                http.get('/api/getLockers/unit/2', () => {
+                    return HttpResponse.json(TestDataCombinationLocker);
+                }),
+            ],
+        },
+    },
     args: {
-        ...KeyLockerTest.args,
+        id: 2,
     },
 };
