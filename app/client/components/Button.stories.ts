7e5c3cd5eed8bc8836cbb3694114c5cd9ab6584c
rrahn <rene.rahn@fu-berlin.de>
2024-06-04 13:40:01 +0200

    Updated the style of the Button component.

--- app/client/components/Button.stories.ts
+++ app/client/components/Button.stories.ts
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 
+import "./Button.css"
+
 import { Button } from "./Button";
 
 const meta = {
@@ -11,9 +13,41 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const PrimaryTest: Story = {
+export const DefaultButton: Story = {
     args: {
-        label: 'Button',
-        onClick: () => console.log('Button clicked'),
+        label: 'Default',
+        onClick: () => console.log('Default clicked'),
     },
 };
+
+export const PrimaryButton: Story = {
+    args: {
+        ...DefaultButton.args,
+        label: 'Primary',
+        color: 'primary',
+    },
+}
+
+export const SecondaryButton: Story = {
+    args: {
+        ...DefaultButton.args,
+        label: 'Secondary',
+        color: 'secondary',
+    },
+}
+
+export const RoundButton: Story = {
+    args: {
+        ...DefaultButton.args,
+        label: 'Round',
+        shape: 'round',
+    },
+}
+
+export const CircleButton: Story = {
+    args: {
+        ...DefaultButton.args,
+        label: 'C',
+        shape: 'circle',
+    },
+}
